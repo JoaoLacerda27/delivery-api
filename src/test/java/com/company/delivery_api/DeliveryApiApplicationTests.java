21b22c28ae839573a39dbc009d9b215d666cfc74
João Lacerda <joaolacerda1862@gmail.com>
2025-12-22 14:13:31 -0300

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/company/delivery_api/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
